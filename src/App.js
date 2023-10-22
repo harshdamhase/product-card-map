d9abb5cb966acfd6c523940067cc0942bb2846d6
Harshda Mhase <harshadamhase12@gmail.com>
2023-10-22 10:13:18 -0400

    folder struture added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  const addValue = () => {
+    setCounter(counter + 1)
+    console.log(counter)
+  }
+
+  const removeValue = () =>
+  {
+    setCounter(counter - 1)
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Current value:</h1>
+
+      <h2>Updated Value: {counter} </h2>
+
+      <button onClick={addValue}>Add</button><br/>
+
+      <button onClick={removeValue}>Remove</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
