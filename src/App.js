2a0aa3dd022bb74f7c3995d60e969ee995f4f090
Harshda Mhase <harshadamhase12@gmail.com>
2023-11-04 06:34:14 -0400

    some changes added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
-import { useState } from "react"
-
+import React from 'react'
 
 function App() {
-
-  const [counter, setCounter] = useState(0);
-
-  const addValue = () => {
-    setCounter(counter + 1)
-    console.log(counter)
-  }
-
-  const removeValue = () =>
-  {
-    setCounter(counter - 1)
-
-  }
   return (
-    <div>
-      <h1>Current value:</h1>
-
-      <h2>Updated Value: {counter} </h2>
-
-      <button onClick={addValue}>Add</button><br/>
-
-      <button onClick={removeValue}>Remove</button>
-    </div>
+    <div>App</div>
   )
 }
 
